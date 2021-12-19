f798ec9558ed12b45137ec44d52f68ebd8b89b3c
Fezz <fullblastfezz@hotmail.com>
2021-12-19 14:34:45 +0000

    Fixed indentation

--- szi_vendingmachine.sql
+++ szi_vendingmachine.sql
@@ -4,5 +4,5 @@ INSERT INTO `items` (`name`, `label`, `weight`) VALUES
 	('lockpick', 'Lockpick', 1),
 	('water', 'Water Bottle', 1),
 	('soda', 'Soda', 1),
-		('coffee', 'Cup Of Coffee', 1)
+	('coffee', 'Cup Of Coffee', 1)
 ;
