b88869d50bdc4c1133a85a2827ba7f1c3e3b457b
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-09-29 23:00:18 +0200

    Dutch translations

---
+++ locales/nl.lua
@@ -0,0 +1,8 @@
+Locales['nl'] = {
+    ['cant_rob'] = '~r~Je kan dit niet overvallen',
+    ['press_stop'] = 'Klik op [DEL] om te stoppen',
+    ['max_amount'] = '~r~Je kan niet meer dragen',
+    ['min_police'] = 'Er moeten minstens ~b~%s agenten~s~ in de stad zijn om te overvallen.',
+    ['no_room'] = '~r~Je kan de buit niet dragen',
+    ['bought'] = 'Je kocht net een ~y~%sx~s~ ~b~%s~s~ voor ~r~$%s~s~',
+}
